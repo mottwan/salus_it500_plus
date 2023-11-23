3eed3fe272cbb1931c7c5f5a922faa51c8ce122d
Ion Motelica <ivanmotelica@x-team.dev>
2023-11-23 16:38:04 +0200

    update domain

--- custom_components/salus_it500_plus/core/const.py
+++ custom_components/salus_it500_plus/core/const.py
@@ -1,21 +1,4 @@
-from homeassistant.const import (
-    # ATTR_BATTERY_LEVEL,
-    # ATTR_TEMPERATURE,
-    CONDUCTIVITY,
-    DEVICE_CLASS_BATTERY,
-    DEVICE_CLASS_TEMPERATURE,
-    PERCENTAGE,
-    TEMP_CELSIUS,
-    )
-
-from homeassistant.components.climate.const import (
-    HVAC_MODE_COOL,
-    # HVAC_MODE_DRY,
-    # HVAC_MODE_FAN_ONLY,
-    HVAC_MODE_HEAT,
-    # HVAC_MODE_HEAT_COOL,
-    HVAC_MODE_OFF
-    )
+from homeassistant.components.climate.const import (HVAC_MODE_HEAT, HVAC_MODE_OFF)
 
 DOMAINS = ['binary_sensor',
            'climate',
@@ -29,7 +12,5 @@ CONF_OCCUPANCY_TIMEOUT = 'occupancy_timeout'
 # Climate
 HVAC_MODES = [HVAC_MODE_HEAT, HVAC_MODE_OFF]
 
-DOMAIN = "salus_thermostat"
+DOMAIN = "salus_it500_plus"
 CONF_DEBUG = "debug"
-
-
